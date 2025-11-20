275ed0464a97abc09eb7c7dade3826c83b671105
RellikEht\Work <rellikeht1992@gmail.com>
2025-11-21 01:30:13 +0200

    general changes

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
-	name: `phpixel.gr`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `phpixel | Κατασκευή Ιστοσελίδων, E-shop & Digital Marketing`,
+	description: `ΚΑΤΑΣΚΕΥΉ WEBSITE, ESHOP, ΑΝΑΠΤΥΞΗ ΕΦΑΡΜΟΓΩΝ, DIGITAL MARKETING.`,
+	url: `https://phpixel.gr`,
+	name: `phpixel.gr`,
 	enableThemeSwitcher: false,
-	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+	showPlug: false,
 };
