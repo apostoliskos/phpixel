a9c69a778390a121e9287dc821ffc84b41ecf163
RellikEht\Work <rellikeht1992@gmail.com>
2025-12-15 13:40:34 +0200

    gen

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,5 @@
 export default {
-	title: `Κατασκευή Ιστοσελίδων, E-shop & Digital Marketing | phpixel`,
+	title: `From idea to pixel and code | phpixel`,
 	description: `ΚΑΤΑΣΚΕΥΉ WEBSITE, ESHOP, ΑΝΑΠΤΥΞΗ ΕΦΑΡΜΟΓΩΝ, DIGITAL MARKETING.`,
 	url: `https://phpixel.gr`,
 	name: `phpixel.gr`,
